4e3bccc15192c010505dd405c765f67dac7c6cb6
ParkYoungWoong <thesecon@gmail.com>
2025-04-15 17:53:32 +0900

    API 환경변수 테스트.

--- api/movie.ts
+++ api/movie.ts
@@ -4,11 +4,11 @@ import fetch from 'node-fetch'
 const { OMDB_API_KEY } = process.env
 
 export default async function handler(request: VercelRequest, response: VercelResponse) {
-  console.log('OMDB_API_KEY', OMDB_API_KEY)
   const { title, page, id } = JSON.parse(request.body as string)
   const url = id
     ? `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&i=${id}&plot=full`
     : `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&page=${page}`
+  console.log('OMDB_API_KEY', OMDB_API_KEY, url)
   const res = await fetch(url)
   const json = await res.json()
   response
